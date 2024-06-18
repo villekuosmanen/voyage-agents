e24b8df16f96761390ca91e5021fd88fff423de0
Ville Kuosmanen <ville@villekuosmanen.com>
2024-06-18 22:20:34 +0100

    allow overriding max tokens

--- voyage_agents/core/manager.py
+++ voyage_agents/core/manager.py
@@ -11,9 +11,10 @@ def log_callback(
     pass
 
 class LlamaManager:
-    def __init__(self, llama: Llama, temperature=0.2):
+    def __init__(self, llama: Llama, temperature=0.2, max_tokens=256):
         self.llm = llama
         self.temperature = temperature
+        self.max_tokens = max_tokens
         llama_log_set(log_callback, ctypes.c_void_p(0))
 
     def query(self, messages, grammar = None):
@@ -21,6 +22,7 @@ class LlamaManager:
             messages=messages,
             response_format={ "type": "text" },
             grammar=grammar,
-            temperature=self.temperature
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
         )
         return res['choices'][0]['message']['content']
